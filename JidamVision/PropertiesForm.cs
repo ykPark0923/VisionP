ff8f694863d230b26a61ba75c4392c06880d3546
ykPark0923 <puklove22@naver.com>
2025-03-06 13:45:55 +0900

    fix: property창 크기 수정

--- JidamVision/PropertiesForm.cs
+++ JidamVision/PropertiesForm.cs
@@ -9,14 +9,66 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WeifenLuo.WinFormsUI.Docking;
 using JidamVision.Core;
+using JidamVision.Property;
 
 namespace JidamVision
 {
+    public enum InspPropType
+    {
+        InspNone = 0,
+        InspBinary,
+        InspMatch,
+        InstTest
+    }
+
     public partial class PropertiesForm : DockContent
     {
         public PropertiesForm()
         {
             InitializeComponent();
+            SetInspType(InspPropType.InspBinary);
+        }
+
+        public void SetInspType(InspPropType inspPropType)
+        {
+            LoadOptionControl(inspPropType);
+        }
+
+        private void LoadOptionControl(InspPropType inspPropType)
+        {
+            // Panel 초기화
+            panelContainer.Controls.Clear();
+            UserControl _inspProp = null;
+
+            // 옵션에 맞는 UserControl 생성
+            switch (inspPropType)
+            {
+                case InspPropType.InspBinary:
+                    _inspProp = new BinaryInspProp();
+                    ((BinaryInspProp)_inspProp).RangeChanged += RangeSlider_RangeChanged;
+                    break;
+                case InspPropType.InspMatch:
+                    _inspProp = new MatchInspProp();
+                    break;
+                default:
+                    MessageBox.Show("유효하지 않은 옵션입니다.");
+                    return;
+            }
+
+            // UserControl을 Panel에 추가
+            if (_inspProp != null)
+            {
+                _inspProp.Dock = DockStyle.Fill; // 패널을 꽉 채움
+                panelContainer.Controls.Add(_inspProp);
+            }
+        }
+
+        private void RangeSlider_RangeChanged(object sender, RangeChangedEventArgs e)
+        {
+            // 속성값을 이용하여 이진화 임계값 설정
+            int lowerValue = e.LowerValue;
+            int upperValue = e.UpperValue;
+            Global.Inst.InspStage.PreView?.SetBinary(lowerValue, upperValue);
         }
     }
 }
--- JidamVision/Property/BinaryInspProp.Designer.cs
+++ JidamVision/Property/BinaryInspProp.Designer.cs
@@ -49,10 +49,10 @@
             this.grpBinary.Controls.Add(this.trackBarUpper);
             this.grpBinary.Controls.Add(this.trackBarLower);
             this.grpBinary.Location = new System.Drawing.Point(4, 4);
-            this.grpBinary.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpBinary.Margin = new System.Windows.Forms.Padding(4);
             this.grpBinary.Name = "grpBinary";
-            this.grpBinary.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.grpBinary.Size = new System.Drawing.Size(434, 297);
+            this.grpBinary.Padding = new System.Windows.Forms.Padding(4);
+            this.grpBinary.Size = new System.Drawing.Size(282, 210);
             this.grpBinary.TabIndex = 0;
             this.grpBinary.TabStop = false;
             this.grpBinary.Text = "이진화";
@@ -63,7 +63,7 @@
             this.chkHighlight.Checked = true;
             this.chkHighlight.CheckState = System.Windows.Forms.CheckState.Checked;
             this.chkHighlight.Location = new System.Drawing.Point(33, 188);
-            this.chkHighlight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkHighlight.Margin = new System.Windows.Forms.Padding(4);
             this.chkHighlight.Name = "chkHighlight";
             this.chkHighlight.Size = new System.Drawing.Size(99, 22);
             this.chkHighlight.TabIndex = 3;
@@ -73,20 +73,20 @@
             // trackBarUpper
             // 
             this.trackBarUpper.Location = new System.Drawing.Point(33, 111);
-            this.trackBarUpper.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBarUpper.Margin = new System.Windows.Forms.Padding(4);
             this.trackBarUpper.Maximum = 255;
             this.trackBarUpper.Name = "trackBarUpper";
-            this.trackBarUpper.Size = new System.Drawing.Size(313, 69);
+            this.trackBarUpper.Size = new System.Drawing.Size(214, 69);
             this.trackBarUpper.TabIndex = 1;
             this.trackBarUpper.Value = 255;
             // 
             // trackBarLower
             // 
             this.trackBarLower.Location = new System.Drawing.Point(33, 34);
-            this.trackBarLower.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBarLower.Margin = new System.Windows.Forms.Padding(4);
             this.trackBarLower.Maximum = 255;
             this.trackBarLower.Name = "trackBarLower";
-            this.trackBarLower.Size = new System.Drawing.Size(313, 69);
+            this.trackBarLower.Size = new System.Drawing.Size(214, 69);
             this.trackBarLower.TabIndex = 0;
             // 
             // BinaryInspProp
@@ -94,9 +94,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.grpBinary);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "BinaryInspProp";
-            this.Size = new System.Drawing.Size(466, 340);
+            this.Size = new System.Drawing.Size(318, 243);
             this.grpBinary.ResumeLayout(false);
             this.grpBinary.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarUpper)).EndInit();
